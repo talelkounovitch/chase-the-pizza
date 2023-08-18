bd42f9054ac643f26e7cec2260bad0c02326d6da
talelkounovitch <142422165+talelkounovitch@users.noreply.github.com>
2023-08-18 10:16:19 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,44 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.startCountdown(100)
+    info.changeScoreBy(1)
+    pizza.setPosition(randint(0, 160), randint(0, 120))
+})
+let pizza: Sprite = null
+pizza = sprites.create(img`
+    . . . . . . b b b b . . . . . . 
+    . . . . . . b 4 4 4 b . . . . . 
+    . . . . . . b b 4 4 4 b . . . . 
+    . . . . . b 4 b b b 4 4 b . . . 
+    . . . . b d 5 5 5 4 b 4 4 b . . 
+    . . . . b 3 2 3 5 5 4 e 4 4 b . 
+    . . . b d 2 2 2 5 7 5 4 e 4 4 e 
+    . . . b 5 3 2 3 5 5 5 5 e e e e 
+    . . b d 7 5 5 5 3 2 3 5 5 e e e 
+    . . b 5 5 5 5 5 2 2 2 5 5 d e e 
+    . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+    . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+    b d 3 2 d 5 5 5 d d d 4 4 . . . 
+    b 5 5 5 5 d d 4 4 4 4 . . . . . 
+    4 d d d 4 4 4 . . . . . . . . . 
+    4 4 4 4 . . . . . . . . . . . . 
+    `, SpriteKind.Food)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 3 3 3 3 . . . . . . 
+    . . . . . . 3 . . 3 . . . . . . 
+    . . . . . . 3 3 3 3 . . . . . . 
+    . . . 3 3 . . 3 3 . . 3 3 . . . 
+    . . . . 3 3 3 3 3 3 3 3 . . . . 
+    . . . 3 3 . . 3 3 . . 3 3 . . . 
+    . . . . . . . 3 3 . . . . . . . 
+    . . . . . . . 3 3 . . . . . . . 
+    . . . . . . . 3 3 . . . . . . . 
+    . . . . . 3 3 3 3 3 3 . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+scene.setBackgroundColor(2)
